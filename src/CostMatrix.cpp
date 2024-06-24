85d9c94a6d97574ef0c46e0f5dd3f9c89a35c1f3
akrasnaya <an.krasnaya@gmail.com>
2024-06-24 16:16:10 +0300

    comments deleted

--- src/CostMatrix.cpp
+++ src/CostMatrix.cpp
@@ -1,46 +1,60 @@
-////
-//// Created by Анастасия Красная on 23.06.2024.
-////
-//
-//#include "CostMatrix.h"
-//#include "utils.h"
-//#include "vector"
-//
-//using namespace utils;
-//
-//class CostMatrix{
-//    protected:
-//    int speed = 2;
-//    int seconds_to_load = 10;
-//    int max_fill_value = 10000;
-//
-//    public:
-//        std::vector<std::vector<int>> route;
-//        std::vector<std::vector<int>> cost_matrix;
-//        CostMatrix(std::vector<std::vector<int>> a) {
-//            route = a;
-//        }
-//
-//
-//    // Функция для подсчета времени от каждой вершины до конца маршрута с учетом времени загрузки и штрафов
-//// Возвращает матрицу стоимости по времени
-//    std::vector<std::vector<int>> create_time_cost_matrix(std::vector<std::vector<int>>& route) {
-//
-//        for (int i = 0; i < route.size(); i++) {
-//            int penalty = 0;
-//            for (int j = 0; j < route.size(); j++) {
-//                if (j > i) {
-//                    time_graph[i][j] = time_dist(route[i], route[j], speed) + seconds_to_load + penalty;
-//                    penalty += route[j][2];
-//                }
-//                else {
-//
-//                    time_graph[i][j] = max_fill_value;
-//                }
-//            }
-//        }
-//        return time_graph;
-//    }
-//
-//
-//};
+#include "vector"
+#include "fstream"
+#include "array"
+
+#include "../include/json.hpp"
+#include "../include/utils.h"
+
+using namespace utils;
+using json = nlohmann::json;
+
+/**
+ * Реализация матрицы эвристик для алгоритмов поиска/планирования
+ */
+
+
+// Загрузка необходимых полей из config
+std::ifstream f("../config.json");
+json config_data = json::parse(f);
+
+class CostMatrix{
+protected:
+    int speed = config_data.at("speed");
+    int seconds_to_load = config_data.at("seconds_to_load");
+    int max_fill_value = config_data.at("max_fill_value");
+
+public:
+    std::vector<std::array<int, 3>> route;
+    std::vector<std::vector<float>> cost_matrix; //
+    CostMatrix(std::vector<std::array<int, 3>> a) {
+        route = a; //единственное поле для конструктора - маршрут робота в виде списка точек
+    }
+
+
+    /**
+    * Функция для подсчета эвристики по времени с учетом параметров задачи и штрафов
+    *
+    * @param[out] двумерная матрица стоимости по времени
+    */
+    std::vector<std::vector<float>> create_time_cost_matrix() {
+        std::vector<std::vector<float>> time_graph (route.size(), std::vector<float> (route.size()));
+        for (int i = 0; i < route.size(); i++) {
+            int penalty = 0;
+            for (int j = 0; j < route.size(); j++) {
+                if (j > i) {
+                    time_graph[i][j] = time_dist(route[i], route[j], speed) + seconds_to_load + penalty;
+                    penalty += route[j][2];
+                }
+                else {
+
+                    time_graph[i][j] = max_fill_value;
+                }
+            }
+        }
+        cost_matrix = time_graph;
+        return cost_matrix;
+    }
+
+
+};
+
